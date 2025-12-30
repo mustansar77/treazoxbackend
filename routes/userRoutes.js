ffc119cb5d4fb8e053848a029591fa8d2690e2da
Mustansar Hussain Tariq <malikmustansarhussain7766@gmail.com>
2025-12-30 07:22:02 +0500

    fixing getMe not define error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, updateBalance } from "../controllers/userController.js";
+import { signup, login, updateBalance,getMe } from "../controllers/userController.js";
 import { authMiddleware, adminMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
