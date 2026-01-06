2ece221bacef234602a5301c4feaa06aebe9aaac
Mustansar Hussain Tariq <malikmustansarhussain7766@gmail.com>
2026-01-06 18:00:08 +0500

    domain updaing link

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "http://localhost:3000",             // Local Next.js frontend
-  "https://www.treazox.com/",  // Vercel frontend
+  "https://www.treazox.com",  // Vercel frontend
 ];
 
 app.use(
